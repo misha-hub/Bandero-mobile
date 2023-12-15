0dd1d43f0fe59e66d2bebac22ee15de0168e8e92
Thor1UA <mihasa7575@gmail.com>
2023-12-16 01:32:47 +0200

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
 print("Hello World")
+x = 2
+y = x + 10
+print(y)
+a = 5
+b = 10
+c = a + 15 - b
+print(c) 
+aaaa = 5 // 2
+print(5 // 2)
+print(aaaa)
+x = 8**3 + 4*(2 + 2)
+print(x)
+# user_age = 7
+user_age = 18
+user_status = "adult"   # 'adult' for users older 18 and 'child'
+a = None
+print(a)
+int_number = 3
+float_number = 3.3
+complex_number = 3.3 + 2j
+print(complex_number)
+hello_string = "Hello"
+world_string = 'World!'
+s = "Hello, World!"
+print(s)
+f = "asdfghjkl"
+print(f)
+s1 = "Hello,"
+s2 = "World!"
+joined_string = s1 + s2
+print(joined_string)
+name = "Misha"
+hello_string = f"Hello, {name}!"
+print(hello_string)
